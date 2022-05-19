10740d9eb78246f91d008a27cbc162081e0e55f6
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-05-19 16:18:47 +0200

    new changes

---
+++ best/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./src/**/*.{js,ts,jsx,tsx}", "./component/**/*.{js,ts,jsx,tsx}"],
+  content: ["./src/**/*.{html,js}"],
+  
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
